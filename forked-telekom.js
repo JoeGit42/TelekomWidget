bb93b0d5f07d824c68c9256e3a67128b7b0b1678
JoeGit42 <joegit42@wienand.net>
2020-11-01 15:34:14 +0100

    Update forked-telekom.js
    Basierend auf dem Fork von [LupusArgentum](https://github.com/LupusArgentum) habe ich ein paar Änderungen vorgenommen:
    
    - Speichern der letzten Sync-Zeit (LupusArgentum hat das bereits parallel implementiert - allerdings leicht anders)
    => Ich speichere direkt im JSON statt einer separaten Datei
    - Schalter um die rote/grüne Einfärbung der restlichen Laufzeit zu unterdrücken: `colorTimeToSignalEndOfMonth`
    - Schalter um die Anzeige im WLAN zu unterdrücken, dass die API nicht erreicht wurde `showIndicationIfAPIOffline`. (orangener Text) => Stattdessen wird nun die letzte Aktualisierung orange eingefärbt, wenn älter als 1 Tag
    - Schalter um das noch verfügbare (statt dem verbrauchten) Volumen anzuzeigen: `showAvailableVolume`
    - Erweiterung der DataPassName-Texte um Texte, die nur aus einem Zeichen bestehen (z.B. bei HIGH Mobile)
    - Erweiterung der Einfärbungen um dynamische (statt statischer) Grenzen. Das verfügbare Volumen wird nun ins Verhältnis gesetzt zur restlichen Laufzeit des Monats
    - Optimierung im Bereich der Texte/Textlängen
    
    ![IMG_0491](https://user-images.githubusercontent.com/14128113/97805206-02d14880-1c55-11eb-907c-b1c4d08396ba.jpeg)
    ![IMG_0492](https://user-images.githubusercontent.com/14128113/97805208-04027580-1c55-11eb-8356-9091424a150c.jpeg)

--- forked-telekom.js
+++ forked-telekom.js
@@ -1,6 +1,12 @@
-// creator: https://github.com/Sillium | changed by me, https://github.com/LupusArgentum
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: pink; icon-glyph: signal;
+// creator: https://github.com/Sillium | changed by https://github.com/LupusArgentum | change again by me, https://github.com/JoeGit42
 const apiUrl = "https://pass.telekom.de/api/service/generic/v1/status"
 const logoIsWanted = (args.widgetParameter == "logo")
+const colorTimeToSignalEndOfMonth = false
+const showIndicationIfAPIOffline = false
+const showAvailableVolume = true
 
 let widget = await createWidget()
 if (!config.runsInWidget) await widget.presentSmall()
@@ -10,34 +16,36 @@ Script.complete()
 async function createWidget(items) {
   let fm = FileManager.local()
   let dir = fm.documentsDirectory()
-  let jsonLocalPath = fm.joinPath(dir, "scriptable-telekom.json")
-  let lastFetchDateLocalPath = fm.joinPath(dir, "lastUpdate.txt")
+  let path = fm.joinPath(dir, "scriptable-telekom.json")
+  let data = 0, api_online = false
 
   const list = new ListWidget()
   list.addSpacer()
- 
+  // no border on the right
+  list.setPadding(10,10,10,0)
+  // Open Telekom-page on klick
+  list.url = "https://pass.telekom.de"
+  
   try {
     let r = new Request(apiUrl)
   // API only answers for mobile Safari
     r.headers = {
       "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1"
     }
-    let data = false, api_online = false, lastFetchDate = false
+    
     try {
       // Fetch data from pass.telekom.de
       data = await r.loadJSON()
-      // Write JSON to local file
-      fm.writeString(jsonLocalPath, JSON.stringify(data, null, 2))
+      // add current time-stamp to JSON
+      //const reqTime = new Date();
+      data.dateStr = new Date().toJSON();
+      // Write JSON to iCloud file
+      fm.writeString(path, JSON.stringify(data, null, 2))
       api_online = true
-      lastFetchDate = new Date()
-      fm.writeString(lastFetchDateLocalPath, lastFetchDate.toString())
     } catch (err) {
-      console.log(err)
-      // Read data from local file
-      if (fm.fileExists(jsonLocalPath) && fm.fileExists(lastFetchDateLocalPath)) {
-        data = JSON.parse(fm.readString(jsonLocalPath), null)
-        lastFetchDate = new Date(fm.readString(lastFetchDateLocalPath, null))
-      } else {
+      // Read data from iCloud file
+      data = JSON.parse(fm.readString(path), null)
+      if (!data || !data.usedPercentage) {
         const errorList = new ListWidget()
         errorList.addText("Please disable WiFi for initial execution.")
         return errorList
@@ -48,7 +56,7 @@ async function createWidget(items) {
     let stack = list.addStack()
     
     // if an additional datapass is booked, display "Pass:" + passname, else display "Datenvolumen:"
-    if (data.passName == "Ihr Datenvolumen" || data.passName == "Ihr Telekom Datentarif") { // you may need to change this! (check your pass name)
+    if (data.passName == "Ihr Datenvolumen" || data.passName == "Ihr Telekom Datentarif" || data.passName.length <= 1) { // you may need to change this! (check your pass name)
       line1 = stack.addText("Datenvolumen")
     } else line1 = stack.addText("Pass: " + data.passName)
     
@@ -59,30 +67,85 @@ async function createWidget(items) {
     const line2 = list.addText(100-data.usedPercentage + "%")
     line2.font = Font.boldSystemFont(36)
     
-    if (data.usedPercentage >= 90) line2.textColor = Color.red()
-    else if (data.usedPercentage >= 75) line2.textColor = Color.orange()
-    else if (data.usedPercentage >= 50) line2.textColor = Color.yellow()
+    // rough calculate remainig percentage of this month 
+    // adjust colors according to what's needed within rest of the month
+    let availableMonthPercentage = (data.remainingSeconds / (31*86400)) * 100  // to ease calculation, it just takes 31 days
+    let bufferPercentage = 5 
+    // e.g. if 20% of month is gone (after the 6th of a month), yellow indication starts at 10% used Volume  (attention: 20% of the month gone means 80% still available)
+    let yellowLevel = (100 - availableMonthPercentage) - (2*bufferPercentage) 
+    if (yellowLevel < 5) yellowLevel = 5 // yellow indication does not earlier than 5%, even at the beginning of a month
+    // e.g. if 20% of month is gone (after the 6th of a month), orange indication starts at 15% used Volume  (attention: 20% of the month gone means 80% still available)
+    let orangeLevel = (100 - availableMonthPercentage) - bufferPercentage 
+    if (orangeLevel < 10) orangeLevel = 10 // orange indication does not earlier than 10%, even at the beginning of a month
+    // e.g. if 20% of month is gone (after the 6th of a month), red indication starts at 25% used Volume  (attention: 20% of the month gone means 80% still available)
+    let redLevel = (100 - availableMonthPercentage) + bufferPercentage
+    if (redLevel > 95) redLevel = 95 // red indication at 95%, even at the end of a month
+
+    if (data.usedPercentage >= redLevel) line2.textColor = Color.red()
+    else if (data.usedPercentage >= orangeLevel) line2.textColor = Color.orange()
+    else if (data.usedPercentage >= yellowLevel) line2.textColor = Color.yellow()
     else line2.textColor = Color.green()
 
-    const line3 = list.addText(data.usedVolumeStr)
-    line3.font = Font.boldSystemFont(13)
+    let line3
+    let line4
     
-    const line4 = list.addText("von " + data.initialVolumeStr + " verbraucht")
-    line4.font = Font.mediumSystemFont(10)
+    if (showAvailableVolume) {
+      let availableVolume = (data.initialVolume - data.usedVolume)/(1024*1024*1024)      
+      let line3pre
+      if (availableVolume <= 1) {  // GB -> MB
+        availableVolume *= 1024
+        line3pre = availableVolume.toFixed(0) + " MB"
+      } else {
+        line3pre = availableVolume.toFixed(2) + " GB"
+      }
+      line3pre = line3pre.replace(".", ",")
+      line3 = list.addText(line3pre)
+    
+      line4 = list.addText("von " + data.initialVolumeStr + " verbleibend")
+    } else {
+     line3 = list.addText(data.usedVolumeStr)
+     line4 = list.addText("von " + data.initialVolumeStr + " verbraucht")
+    }
+    line3.font = Font.boldSystemFont(13)
+    line4.font = Font.mediumSystemFont(10)      
     
     list.addSpacer()
     let line5
 	 
     // alt text on line5 if local data instead of Telekom API data:
-    if (api_online) {
+    if (api_online || !showIndicationIfAPIOffline) {
       let plan = (data.remainingSeconds ? "prepaid" : data.remainingTimeStr ? "postpaid" : "")
       switch (plan) {
         case "prepaid":
           let days = Math.floor(data.remainingSeconds / 86400)
           let hours = Math.floor((data.remainingSeconds % 86400) / 3600)
-          line5 = list.addText("noch " + days + "d " + hours + "h übrig")
+          let daysStr = ""
+          let hoursStr = ""
+          
+          // To differentiate between single and multiple days/hours
+          if (days > 0) {
+            if (days == 1) {
+              daysStr += days + " Tag "
+            } else {
+              daysStr += days + " Tage "
+            }
+          }
+          
+          if (hours > 0) {
+            if (hours == 1) {
+               daysStr += hours + ((days > 0 && !showAvailableVolume) ? " Std." : " Stunde")
+            } else {
+               daysStr += hours + ((days > 0 && !showAvailableVolume) ? " Std." : " Stunden")  // if available Volume is shown, text can be a bit longer
+            }
+          }
+              
+          if (days == 0 && hours == 0) {
+                (showAvailableVolume) ? line5 += "für < 1 Stunde" :  line5 += "< 1 Stunde übrig"
+          } else {
+             (showAvailableVolume) ? line5 = list.addText("für " + daysStr + hoursStr) : line5 = list.addText(daysStr + hoursStr + " übrig")
+          }
           line5.font = Font.mediumSystemFont(12)
-          line5.textColor = (days < 3 ? Color.red() : Color.green())
+          if (colorTimeToSignalEndOfMonth)  line5.textColor = (days < 3 ? Color.red() : Color.green())
           break;
       
         case "postpaid":
@@ -97,21 +160,22 @@ async function createWidget(items) {
         line5.textColor = Color.orange()
     }
     
-    // Add time (and date) of last data fetch
-    const df = new DateFormatter()
-    const wasFetchedToday = (lastFetchDate.getDate() == new Date().getDate())
-    df.dateFormat = (wasFetchedToday ? "hh:mm" : "dd.MM. HH:mm")
-    
-    let timeLabel = list.addText("aktualisiert " + df.string(lastFetchDate))
-    timeLabel.font = Font.mediumSystemFont(9)
-    timeLabel.textColor = Color.lightGray()
-    list.addSpacer()
-      
+  
   } catch (err) {
-    console.log(err)
     list.addText("Error fetching JSON from https://pass.telekom.de/api/service/generic/v1/status")
   }
- 
+
+  // Add time of last (successful) widget refresh:
+  const currentTime = new Date()  // current timestamp
+  let syncTime = new Date(data.dateStr) // timestamp of last sync in cellular environment
+  const df = new DateFormatter()
+  const wasFetchedToday = (currentTime.getDate() == syncTime.getDate() && currentTime.getMonth() == syncTime.getMonth() )  
+  df.dateFormat = (wasFetchedToday ? "HH:mm" : "dd.MM. HH:mm")  
+  let timeLabel = list.addText("aktualisiert " + df.string(syncTime))
+  timeLabel.font = Font.mediumSystemFont(10)
+  // If APIOffline indication is not shown, the text is marked orange, if last sync is older than 1 day (86400 seconds * milliseconds)
+  timeLabel.textColor = (!showIndicationIfAPIOffline && (currentTime - syncTime) > (86400*1000)) ? Color.orange() : Color.lightGray()
+  list.addSpacer()
   return list
 }
 
@@ -135,6 +199,7 @@ async function addLogoToLine1(stack) {
     stackImage.rightAlignImage()
     stackImage.imageSize = new Size(16,16)
   }
+  stack.addText("   ")  // as there's no border on the right, but some spaces to get distance
 }
 
 async function loadImage(imgUrl) {
